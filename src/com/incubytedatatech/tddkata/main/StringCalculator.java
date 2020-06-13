c04091663396a686c3bca72a91aea8db0813231c
bhaskar <bhaskar26.pawar@gmail.com>
2020-06-13 18:05:30 +0530

    refactored code and seventh test case passed

--- src/com/incubytedatatech/tddkata/main/StringCalculator.java
+++ src/com/incubytedatatech/tddkata/main/StringCalculator.java
@@ -5,6 +5,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import com.incubytedatatech.tddkata.exceptions.NegativeNumberException;
+
 public class StringCalculator {
 
 	public Integer add(String numbers) {
@@ -12,25 +14,24 @@ public class StringCalculator {
 		if ("".equals(numbers.trim())) {
 			return 0;
 		}
-		else {
-			Pattern pattern = Pattern.compile("\\d+");
-			Matcher matcher = pattern.matcher(numbers);
-			List <Integer> intNumbers = matcher.results()
-					.mapToInt(result -> Integer.valueOf(result.group()))
-					.boxed()
-					.collect(Collectors.toList());
-			if (intNumbers.size() == 1) {
-				return intNumbers.get(0);
-			}
-			else {
-				
-				return intNumbers.stream().mapToInt(number -> number)
-							  .sum();
-			}
-			
+		
+		Pattern pattern = Pattern.compile("-?[0-9]\\d*");
+		Matcher matcher = pattern.matcher(numbers);
+		List <Integer> intNumbers = matcher.results()
+				.mapToInt(result -> Integer.valueOf(result.group()))
+				.boxed()
+				.collect(Collectors.toList());
+		if (intNumbers.size() == 1 && intNumbers.get(0) < 0) {
+			throw new NegativeNumberException("no negatives allowed = " + intNumbers.get(0).toString());
 		}
-		
-		
+		else if (intNumbers.size() == 1) {
+			return intNumbers.get(0);
+		}
+		else {	
+			return intNumbers.stream().mapToInt(number -> number)
+							  .sum();
+		}
+			
 	}
 
 }
