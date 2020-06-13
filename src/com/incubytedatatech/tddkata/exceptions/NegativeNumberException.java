b1b88217dfa1156227aae4cfc20d8d56a719a5d6
bhaskar <bhaskar26.pawar@gmail.com>
2020-06-13 17:43:37 +0530

    created new class for seventh test case

---
+++ src/com/incubytedatatech/tddkata/exceptions/NegativeNumberException.java
@@ -0,0 +1,13 @@
+package com.incubytedatatech.tddkata.exceptions;
+
+public class NegativeNumberException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NegativeNumberException (String message) {
+		super(message);
+	}
+}
