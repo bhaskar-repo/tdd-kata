4861ee2b13b2de584ac9ddccf4eb6eba88ca4273
bhaskar <bhaskar26.pawar@gmail.com>
2020-06-13 16:34:10 +0530

    started writing a first test case

--- src/com/incubytedatatech/tddkata/tests/StringCalculatorTest.java
+++ src/com/incubytedatatech/tddkata/tests/StringCalculatorTest.java
@@ -1,5 +1,13 @@
 package com.incubytedatatech.tddkata.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
 
+	@Test
+	public void add_shouldReturn0WhenNumbersIsEmpty () {
+		StringCalculator stringCalculator = new StringCalculator();
+	}
 }
