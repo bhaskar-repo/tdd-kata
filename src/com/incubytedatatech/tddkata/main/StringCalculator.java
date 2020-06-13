7a974bb231fea6409dec7b09dbdab4e677b02fdc
bhaskar <bhaskar26.pawar@gmail.com>
2020-06-13 16:38:38 +0530

    added a missing method add

--- src/com/incubytedatatech/tddkata/main/StringCalculator.java
+++ src/com/incubytedatatech/tddkata/main/StringCalculator.java
@@ -2,4 +2,8 @@ package com.incubytedatatech.tddkata.main;
 
 public class StringCalculator {
 
+	public Integer add(String string) {
+		return null;
+	}
+
 }
--- src/com/incubytedatatech/tddkata/tests/StringCalculatorTest.java
+++ src/com/incubytedatatech/tddkata/tests/StringCalculatorTest.java
@@ -4,10 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.incubytedatatech.tddkata.main.StringCalculator;
+
 public class StringCalculatorTest {
 
 	@Test
 	public void add_shouldReturn0WhenNumbersIsEmpty () {
 		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(0, stringCalculator.add(""));
 	}
 }
