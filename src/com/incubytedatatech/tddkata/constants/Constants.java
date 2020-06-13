6e5381161ccc2d693243806cee20b218099a3810
bhaskar <bhaskar26.pawar@gmail.com>
2020-06-13 18:44:38 +0530

    created new class constants

---
+++ src/com/incubytedatatech/tddkata/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.incubytedatatech.tddkata.constants;
+
+public class Constants {
+	
+	public static final String ONLYNUMBERS= "-?[0-9]\\d*";
+	public static final String EMPTYSTRING= "";
+	public static final String NEGATIVENUMBERSMSG = "no negatives allowed";
+	
+}
